a234f556f6004d6956c7fedef917d23814b99622
OthoCrow <othocrow69@gmail.com>
2025-05-29 12:44:13 +1200

    Made string_checker for adaptible

--- MMF/C_02_string_checker_v2.py
+++ MMF/C_02_string_checker_v2.py
@@ -14,7 +14,7 @@ def string_checker(question, valid_answers=("yes", "no"), num_letters=1):
             elif response == item[:num_letters]:
                 return item
 
-        print(f"Please choose yes or no")
+        print(f"Please choose from {valid_answers}")
 
 # main routine 
 payment_list = ('cash', 'credit')
