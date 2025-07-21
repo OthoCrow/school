d58d3aa5feeb4d97d6563317e2f95c4b52a81bce
OthoCrow <othocrow69@gmail.com>
2025-07-22 08:57:14 +1200

    Created encode function

---
+++ Assessment/C_04_encode.py
@@ -0,0 +1,19 @@
+# Functions
+def encode(text):
+    output = ""
+    for c in text:
+        if c in alphabet:
+            index = alphabet.index(c)
+            new_index = index + shift
+            output += alphabet[new_index]
+        else:
+            output += "?"
+    return output
+
+# Variables
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+shift = 3
+text = "hello"
+
+# Main routine
+print(encode(text))
