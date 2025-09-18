ffa8996b7439e839b9f8d705029501dd9f01626d
OthoCrow <othocrow69@gmail.com>
2025-09-18 13:06:39 +1200

    Delete Assessment (copy 1) directory

--- Assessment (copy 1)/C_01_make_statement.py
+++
@@ -1,8 +0,0 @@
-# Functions go here
-def make_statement(statement, decoration):
-    # Emphasises headings by adding decorationg to the start and end
-
-    print(f"{decoration * 3} {statement} {decoration * 3}\n")
-
-# Main routine goes here
-make_statement("Instructions", "ℹ️")
