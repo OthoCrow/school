292a5baaf29399825b0966e48c0beb3c672c785e
OthoCrow <othocrow69@gmail.com>
2025-10-27 10:38:46 +1300

    Removed duplicate code, fixed some logic bugs, restructured round_transformation and added decryption (doesnt work)

--- Assessment_resit/B_02_AES.py
+++ Assessment_resit/B_02_AES.py
@@ -34,8 +34,42 @@ SBOX = [
     0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
 ]
 
-RCON = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36]
+INV_SBOX = [
+    0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38,
+    0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+    0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87,
+    0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+    0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d,
+    0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+    0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2,
+    0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+    0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16,
+    0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+    0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda,
+    0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+    0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a,
+    0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+    0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02,
+    0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+    0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea,
+    0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+    0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85,
+    0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+    0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89,
+    0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+    0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20,
+    0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+    0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31,
+    0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+    0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d,
+    0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+    0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0,
+    0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26,
+    0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+]
 
+RCON = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36]
 
 
 # Functions go here
@@ -95,7 +129,7 @@ def int_check(question, low, high):
 
 def to_state():
     # Converts plaintext to hexidecimal bytes
-    plaintext = validate_input("Enter plaintext: ", 100)
+    plaintext = validate_input("Enter plaintext: ", None, 100)
     utf8_bytes = plaintext.encode("utf-8")
     deci_bytes = list(utf8_bytes)
     # Adds padding so each block is 16 bytes
@@ -111,31 +145,12 @@ def to_state():
         block_num = block_index // 16
         block = padded_bytes[block_index:block_index+16]
         rows = {
-            "row0": block[0:4],
-            "row1": block[4:8],
-            "row2": block[8:12],
-            "row3": block[12:16],
+            "row0": [block[0], block[4], block[8], block[12]],
+            "row1": [block[1], block[5], block[9], block[13]],
+            "row2": [block[2], block[6], block[10], block[14]],
+            "row3": [block[3], block[7], block[11], block[15]],
         }
-        block_dicts[f"block{block_num}"] = rows
-    return block_dicts    # Converts plaintext to hexidecimal bytes
-    plaintext = validate_input("Enter plaintext: ", 100)
-    utf8_bytes = plaintext.encode("utf-8")
-    deci_bytes = list(utf8_bytes)
-    # Adds padding so each block is 16 bytes
-    length = len(deci_bytes)
-    padding_amount = 16 - length % 16
-    padding = [padding_amount] * padding_amount
-    padded_bytes = deci_bytes + padding
-    block_dicts = {}
-    for block_index in range(0, len(padded_bytes), 16):
-        block_num = block_index // 16
-        block = padded_bytes[block_index:block_index+16]
-        rows = {
-            "row0": block[0:4],
-            "row1": block[4:8],
-            "row2": block[8:12],
-            "row3": block[12:16],
-        }
+
         block_dicts[f"block{block_num}"] = rows
     return block_dicts
 
@@ -149,8 +164,9 @@ def g(word, round_num):
     word[0] ^= RCON[round_num]
     return word
 
-# Expands original 128 bit key into 44 round keys
+
 def expand_keys():
+    # Expands original 128 bit key into 44 round keys
     key = list(validate_input("Enter 16 character key: ", 16).encode("utf-8"))
     # Splits key into "words"
     words = [key[i:i +4] for i in range(0, 16, 4)]
@@ -172,8 +188,9 @@ def expand_keys():
         keys[f"key{round_num}"] = w0 + w1 + w2 + w3
     return keys
 
-# GF(2^8) multiplication
+
 def gmul(a, b):
+    # GF(2^8) multiplication
     p = 0
     for i in range(8):
         if b & 1:
@@ -187,50 +204,154 @@ def gmul(a, b):
     return p
 
 
-def round_transformation(state, mixcolumns):
-    new_state = {}
-    for block_name, rows in state.items():
+def sub_bytes(block_name, rows):
+    subbed = {}
+    for r, row in rows.items():
+        new_row = []
+        for b in row:
+            new_row.append(SBOX[b])
+        subbed[r] = new_row
+    return subbed
 
-        # SubBytes
-        subbed = {}
-        for r, row in rows.items():
-            new_row = []
-            for b in row:
-                new_row.append(SBOX[b])
-            subbed[r] = new_row
 
-        # ShiftRows
-        shifted = {}
-        shifted["row0"] = subbed["row0"]
-        shifted["row1"] = subbed["row1"][1:] + subbed["row1"][:1]
-        shifted["row2"] = subbed["row2"][2:] + subbed["row2"][:2]
-        shifted["row3"] = subbed["row3"][3:] + subbed["row3"][:3]
+def shift_rows(subbed):
+    shifted = {}
+    shifted["row0"] = subbed["row0"]
+    shifted["row1"] = subbed["row1"][1:] + subbed["row1"][:1]
+    shifted["row2"] = subbed["row2"][2:] + subbed["row2"][:2]
+    shifted["row3"] = subbed["row3"][3:] + subbed["row3"][:3]
+    return shifted
 
-        # MixColumns
-        mixed = {"row0": [], "row1": [], "row2": [], "row3": []}
-        for c in range(4):
-            a0 = shifted["row0"][c]
-            a1 = shifted["row1"][c]
-            a2 = shifted["row2"][c]
-            a3 = shifted["row3"][c]
 
-            if mixcolumns == True:
-                col0 = gmul(a0, 2) ^ gmul(a1, 3) ^ gmul(a2, 1) ^ gmul(a3, 1)
-                col1 = gmul(a0, 1) ^ gmul(a1, 2) ^ gmul(a2, 3) ^ gmul(a3, 1)
-                col2 = gmul(a0, 1) ^ gmul(a1, 1) ^ gmul(a2, 2) ^ gmul(a3, 3)
-                col3 = gmul(a0, 3) ^ gmul(a1, 1) ^ gmul(a2, 1) ^ gmul(a3, 2)
+def mix_columns(shifted, block_name, mixcolumns=True):
+    mixed = {"row0": [], "row1": [], "row2": [], "row3": []}
+    for c in range(4):
+        a0 = shifted["row0"][c]
+        a1 = shifted["row1"][c]
+        a2 = shifted["row2"][c]
+        a3 = shifted["row3"][c]
+
+        if mixcolumns == True:
+            col0 = gmul(a0, 2) ^ gmul(a1, 3) ^ gmul(a2, 1) ^ gmul(a3, 1)
+            col1 = gmul(a0, 1) ^ gmul(a1, 2) ^ gmul(a2, 3) ^ gmul(a3, 1)
+            col2 = gmul(a0, 1) ^ gmul(a1, 1) ^ gmul(a2, 2) ^ gmul(a3, 3)
+            col3 = gmul(a0, 3) ^ gmul(a1, 1) ^ gmul(a2, 1) ^ gmul(a3, 2)
+        else:
+            col0 = a0
+            col1 = a1
+            col2 = a2
+            col3 = a3
+
+        mixed["row0"].append(col0)
+        mixed["row1"].append(col1)
+        mixed["row2"].append(col2)
+        mixed["row3"].append(col3)
+
+    return mixed
+
+
+def inv_shift_rows(rows):
+    shifted = {}
+    shifted["row0"] = rows["row0"]
+    shifted["row1"] = rows["row1"][-1:] + rows["row1"][:-1]
+    shifted["row2"] = rows["row2"][-2:] + rows["row2"][:-2]
+    shifted["row3"] = rows["row3"][-3:] + rows["row3"][:-3]
+    return shifted
+
+
+def inv_sub_bytes(shifted):
+    subbed = {}
+    for r, row in shifted.items():
+        new_row = []
+        for b in row:
+            new_row.append(INV_SBOX[b])
+        subbed[r] = new_row
+    return subbed
+
+
+def inv_mix_columns(subbed, block_name, mixcolumns=True):
+    mixed = {"row0": [], "row1": [], "row2": [], "row3": []}
+    for c in range(4):
+        a0 = subbed["row0"][c]
+        a1 = subbed["row1"][c]
+        a2 = subbed["row2"][c]
+        a3 = subbed["row3"][c]
+
+        if mixcolumns == True:
+            col0 = gmul(a0, 14) ^ gmul(a1, 11) ^ gmul(a2, 13) ^ gmul(a3, 9)
+            col1 = gmul(a0, 9) ^ gmul(a1, 14) ^ gmul(a2, 11) ^ gmul(a3, 13)
+            col2 = gmul(a0, 13) ^ gmul(a1, 9) ^ gmul(a2, 14) ^ gmul(a3, 11)
+            col3 = gmul(a0, 11) ^ gmul(a1, 13) ^ gmul(a2, 9) ^ gmul(a3, 14)
+        else:
+            col0 = a0
+            col1 = a1
+            col2 = a2
+            col3 = a3
+
+        mixed["row0"].append(col0)
+        mixed["row1"].append(col1)
+        mixed["row2"].append(col2)
+        mixed["row3"].append(col3)
+        
+    return mixed
+
+
+def round_transformation(state, decrypt, keys):
+    # Encryption
+    if decrypt == False:
+        for round_num in range(1, 11):
+            temp_state = {}
+            # Do mix_columns?
+            if round_num == 10:
+                mixcolumns = False
+            else: 
+                mixcolumns = True
+            for block_name, rows in state.items():
+
+                # Sub bytes
+                subbed = sub_bytes(block_name, rows)
+
+                # Shift Rows
+                shifted = shift_rows(subbed)
+
+                # Mix Columns
+                mixed = mix_columns(shifted, block_name, mixcolumns)
+
+                # Add Round Key
+                keyed = add_round_key({block_name: mixed}, keys[f'key{round_num}'])
+
+                temp_state.update(keyed)
+            # Use for next round
+            state = temp_state
+        # Use for output
+        new_state = state
+
+    # Decryption
+    else:
+        for round_num in range(10, 0, -1):
+            temp_state = {}
+            # Do inv_mix_columns?
+            if round_num == 1:
+                mixcolumns = False
             else:
-                col0 = a0
-                col1 = a1
-                col2 = a2
-                col3 = a3
+                mixcolumns = True 
 
-            mixed["row0"].append(col0)
-            mixed["row1"].append(col1)
-            mixed["row2"].append(col2)
-            mixed["row3"].append(col3)
+            for block_name, rows in state.items():                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+                # Inv Shift Rows
+                shifted = inv_shift_rows(rows)
 
-        new_state[block_name] = mixed
+                # Inv Sub Bytes
+                subbed = inv_sub_bytes(shifted)
+
+                # Add Round Key
+                keyed = add_round_key({block_name: subbed}, keys[f'key{round_num -1}'])
+                
+                # Inv Mix Columns
+                mixed = inv_mix_columns(keyed[block_name], block_name, mixcolumns)
+
+                temp_state[block_name] = mixed
+            state = temp_state
+        new_state = state
 
     return new_state
 
@@ -242,22 +363,25 @@ def add_round_key(state, round_key):
         for r in range(4):
             new_row = []
             for c in range(4):
-                key_index = r * 4 + c
+                key_index = r + 4 * c
                 new_row.append(rows[f"row{r}"][c] ^ round_key[key_index])
             new_rows[f"row{r}"] = new_row
         new_state[block_name] = new_rows
     return new_state
 
 
-def validate_input(prompt, length=None):
+def validate_input(prompt, length=None, max_length=None):
     # Checks that input is ascii characters
     while True:
-        response = input(prompt).strip().lower()
+        response = input(prompt).strip()
         if response.isascii() == False:
             print("Please enter characters only from the ascii character set: ")
             continue
-        if length != None and len(response) > length:
-            print(f"Please enter {length} characters or less: ")
+        if length != None and len(response) != length:
+            print(f"Please enter {length} characters: ")
+            continue
+        if max_length != None and len(response) > max_length:
+            print(f"Please enter {max_length} characters or less: ")
             continue
         return response
 
@@ -265,26 +389,28 @@ def validate_input(prompt, length=None):
 # Main routine
 make_statement("ENCRYPTION", "#", 5)
 print(
-        f"""This is a program that for encrypting text. Encrypting is conveting plain text into ciphertext (or a code), like spies do.
+        f"""This is a program for encrypting text. Encrypting is conveting plain text into ciphertext (or a code), like spies do.
         That means that you can enter a word or multiple words and a key to encrypt it with.
         In the terms of encryption, a key is much like a physical key, you can lock and unlock your text with it.
         Enjoy!\n\n"""
 )
 
 instructions()
+decrypt = string_checker("Would you like to encrypt or decrypt?: ", valid_answers=("encrypt", "decrypt"))
+if decrypt == "decrypt":
+    decrypt = True
+else:
+    decrypt = False
 
 keys = expand_keys()
 state_array = to_state()
-state_array = add_round_key(state_array, keys['key0'])
 
-for round_num in range(1, 11):
-    if round_num < 10:
-        mixcolumns = True
-    else: 
-        mixcolumns = False
+if decrypt:
+    state_array = add_round_key(state_array, keys['key10'])
+else:
+    state_array = add_round_key(state_array, keys['key0'])
 
-    state_array = round_transformation(state_array, mixcolumns)
-    state_array = add_round_key(state_array, keys[f'key{round_num}'])
+state_array = round_transformation(state_array, decrypt, keys)
 
 output = []
 for block_name in state_array:
