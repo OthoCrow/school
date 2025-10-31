aaa138f798a4459133028afce76dfdf42a1f2753
OthoCrow <othocrow69@gmail.com>
2025-10-31 16:57:10 +1300

    feat make statment: made more flexible

--- Assessment_resit/C_01_make_statement.py
+++ Assessment_resit/C_01_make_statement.py
@@ -1,8 +1,7 @@
 # Functions go here
-def make_statement(statement, decoration):
-    # Emphasises headings by adding decorationg to the start and end
-
-    print(f"{decoration * 3} {statement} {decoration * 3}\n")
+def make_statement(statement, decoration, amount=3):
+    # Emphasise headings by adding decoration to the start and end.
+    print(f"{decoration * amount} {statement} {decoration * amount}\n")
 
 # Main routine goes here
 make_statement("Instructions", "ℹ️")
