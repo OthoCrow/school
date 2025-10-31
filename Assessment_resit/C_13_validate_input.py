72fb846d2d661f5afd020c1831d73f2f4f0b3746
OthoCrow <othocrow69@gmail.com>
2025-10-31 17:36:29 +1300

    feat validate_input: allows optional specific length and max_length

--- Assessment_resit/C_13_validate_input.py
+++ Assessment_resit/C_13_validate_input.py
@@ -1,11 +1,18 @@
-def validate_input(prompt, length=None):
-    # Checks that input is alphabetical characters
-    response = input(prompt).strip().lower()
+def validate_input(prompt, length=None, max_length=None):
+    # Checks that input is ascii characters (optionally specific lengths)
     while True:
+        response = input(prompt).strip()
         if response.isascii() == False:
-            return "Please enter characters only from the ascii character set: "
-        if length != None and len(response) > length:
-            return f"Please enter {length} characters or less: "
+            print("Please enter characters only from the ascii character set: ")
+            continue
+        # Is equal to defined length
+        if length != None and len(response) != length:
+            print(f"Please enter {length} characters: ")
+            continue
+        # Is less than max length
+        if max_length != None and len(response) > max_length:
+            print(f"Please enter {max_length} characters or less: ")
+            continue
         return response
 
 
