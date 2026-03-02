17fb8390b0cdfdb86dc70f3a4584c0dabf7817ab
OthoCrow <othocrow69@gmail.com>
2026-03-02 15:04:00 +1300

    Created temp gui  v3

---
+++ level3/programming/practice/c_01_temp_gui_v3.py
@@ -0,0 +1,90 @@
+from tkinter import *
+import all_constants as c
+
+class Converter():
+    # Temperature conversion tool (C F)
+
+    def __init__(self):
+        # Temperature converter gui
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame, text="Temperature Converter", font=("Arial", "16", "bold"))
+
+        self.temp_heading.grid(row=0)
+
+        instructions = "Please enter a temperature below and then press on of the buttons to convert it from Centigrade to Fahrenheit."
+        self.temp_instructions = Label(self.temp_frame, text=instructions, wraplength=250, width=40, justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry =Entry(self.temp_frame, font=("Arial", "14"))
+        self.temp_entry.grid(row=2, padx=10, pady=10)
+
+        error = "Please enter a number"
+        self.answer_error = Label(self.temp_frame, text=error, fg="#004c99")
+        self.answer_error.grid(row=3)
+
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        # Button list (button text | bg colour | command | row | column)
+        button_details_list = [
+            ["To Celsius", "#990099", lambda:self.check_temp(c.ABS_ZERO_FAHRENHEIT), 0, 0],
+            ["To Fahrenheit", "#009900", lambda:self.check_temp(c.ABS_ZERO_CELSIUS), 0, 1],
+            ["Help / Info", "#CC6600", "", 1, 0],
+            ["History", "#004C99", "", 1, 1] 
+        ]
+
+        # list hold buttons
+        self.button_ref_list = []
+
+        for item in button_details_list:
+            self.make_button = Button(self.button_frame, text=item[0], bg=item[1], fg='#ffffff', font=("Arial", "12", "bold"), width="12", command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+
+            self.button_ref_list.append(self.make_button)
+
+        self.to_history_button = self.button_ref_list[3].config(state=DISABLED)
+
+    def check_temp(self, min_temp):
+        # Checks temperature is valid and either invokes calculation or shows error
+
+        print("Min Temp: ", min_temp)
+
+        to_convert = self.temp_entry.get()
+        print("to convert", to_convert)
+        
+        self.answer_error.config(fg="#004c99")
+        self.temp_entry.config(bg="#ffffff")
+
+
+        try:
+            to_convert = float(to_convert)
+            if to_convert >= min_temp:
+                error = ""
+                self.convert(min_temp)
+            else:
+                error = f"Cannot enter a number below {min_temp}"
+        except ValueError: 
+            error = "Please enter a number"
+
+        if error != "":
+            self.answer_error.config(text=error, fg="#9C0000")
+            self.temp_entry.config(bg="#F4CCCC")
+            self.temp_entry.delete(0, END)
+
+    def convert(self, min_temp):
+
+        if min_temp == c.ABS_ZERO_CELSIUS:
+            self.answer_error.config(text="Convertin to F")
+        else:
+            self.answer_error.config(text="Convertin to C")
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
